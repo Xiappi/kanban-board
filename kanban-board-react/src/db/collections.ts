ea846829a2884d8113bfb4da8ad71f4509f7a802
Adam_Mattatall <mattatalladam@gmail.com>
2025-11-11 18:05:37 -0400

    add types for netlify

--- kanban-board-react/src/db/collections.ts
+++ kanban-board-react/src/db/collections.ts
@@ -3,15 +3,21 @@ import { SwimlaneModelConverter } from "./SwimlaneConverter";
 import { db } from "../auth/firebase";
 import { BoardModelConverter } from "./BoardModelConverter";
 import { ItemModelConverter } from "./ItemModelConverter";
+import type { SwimlaneModel } from "../models/Swimlane";
+import type { BoardModel } from "../models/BoardModel";
+import type { ItemModel } from "../models/ItemModel";
 
-export const swimlaneCollectionRef = collection(db, "swimlanes").withConverter(
-  SwimlaneModelConverter
-);
+export const swimlaneCollectionRef = collection(
+  db,
+  "swimlanes"
+).withConverter<SwimlaneModel>(SwimlaneModelConverter);
 
-export const boardCollectionRef = collection(db, "boards").withConverter(
-  BoardModelConverter
-);
+export const boardCollectionRef = collection(
+  db,
+  "boards"
+).withConverter<BoardModel>(BoardModelConverter);
 
-export const itemsCollectionRef = collection(db, "items").withConverter(
-  ItemModelConverter
-);
+export const itemsCollectionRef = collection(
+  db,
+  "items"
+).withConverter<ItemModel>(ItemModelConverter);
